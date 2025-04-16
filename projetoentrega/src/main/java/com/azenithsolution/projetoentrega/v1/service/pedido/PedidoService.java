b58565a36eb117206c9142d1c2dc40792f263552
João <joao.melo@sptech.school>
2025-04-16 19:31:09 -0300

    [FEAT] :: Added PedidoRepository and PedidoService

--- projetoentrega/src/main/java/com/azenithsolution/projetoentrega/v1/service/pedido/PedidoService.java
+++ projetoentrega/src/main/java/com/azenithsolution/projetoentrega/v1/service/pedido/PedidoService.java
@@ -1,4 +1,24 @@
 package com.azenithsolution.projetoentrega.v1.service.pedido;
 
+import com.azenithsolution.projetoentrega.v1.model.Pedido;
+import com.azenithsolution.projetoentrega.v1.repository.PedidoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PedidoService {
+    private final PedidoRepository pedidoRepository;
+
+    public PedidoService(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    public Pedido salvar(Pedido pedido){
+        return pedidoRepository.save(pedido);
+    }
+
+    public List<Pedido> listarPedidos(){
+        return pedidoRepository.findAll();
+    }
 }

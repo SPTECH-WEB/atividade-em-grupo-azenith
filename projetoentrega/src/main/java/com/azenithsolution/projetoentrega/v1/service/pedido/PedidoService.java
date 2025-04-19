2a27f1319b8ea996b38270908ba1523fd6e0c233
HenriqueSericov <HenriqueSericovM@outlook.com>
2025-04-19 14:54:17 -0300

    Subindo Observer completo

--- projetoentrega/src/main/java/com/azenithsolution/projetoentrega/v1/service/pedido/PedidoService.java
+++ projetoentrega/src/main/java/com/azenithsolution/projetoentrega/v1/service/pedido/PedidoService.java
@@ -2,6 +2,7 @@ package com.azenithsolution.projetoentrega.v1.service.pedido;
 
 import com.azenithsolution.projetoentrega.v1.model.Pedido;
 import com.azenithsolution.projetoentrega.v1.repository.PedidoRepository;
+import com.azenithsolution.projetoentrega.v1.service.frete.observer.PedidoObserver;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,16 +10,20 @@ import java.util.List;
 @Service
 public class PedidoService {
     private final PedidoRepository pedidoRepository;
+    private final List<PedidoObserver> pedidoObservers;
 
-    public PedidoService(PedidoRepository pedidoRepository) {
+    public PedidoService(PedidoRepository pedidoRepository, List<PedidoObserver> pedidoObservers) {
         this.pedidoRepository = pedidoRepository;
+        this.pedidoObservers = pedidoObservers;
     }
 
-    public Pedido salvar(Pedido pedido){
-        return pedidoRepository.save(pedido);
+    public Pedido salvar(Pedido pedido) {
+        Pedido pedidoSalvo = pedidoRepository.save(pedido);
+        pedidoObservers.forEach(observer -> observer.notificar(pedidoSalvo));
+        return pedidoSalvo;
     }
 
-    public List<Pedido> listarPedidos(){
+    public List<Pedido> listarPedidos() {
         return pedidoRepository.findAll();
     }
 }
